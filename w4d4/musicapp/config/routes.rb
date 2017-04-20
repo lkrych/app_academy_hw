4f8a4524d7d788902722820779a3d42805110ab9
Leland Krych <Leland.Krych@gmail.com>
2017-04-20 16:19:51 -0700

    Create routes for music models

--- w4d4/musicapp/config/routes.rb
+++ w4d4/musicapp/config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :destroy]
 
-  resources :bands
+  resources :bands do
     resource :albums, only: [:new]
+  end
 
-  resources :albums, only: [:create, :new, :edit, :show, :update, :destroy]
+  resources :albums, only: [:create, :new, :edit, :show, :update, :destroy] do
     resource :tracks, only: [:new]
+  end
+
+  resources :tracks, only: [:create, :new, :edit, :show, :update, :destroy]
 end
