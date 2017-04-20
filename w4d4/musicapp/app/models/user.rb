43252454efe8823d9588759725536b2e7fc7b997
Leland Krych <Leland.Krych@gmail.com>
2017-04-20 12:10:41 -0700

    Add validations to User model

--- w4d4/musicapp/app/models/user.rb
+++ w4d4/musicapp/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :email,  presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank'}
+  validates :password, length: { minimum: 6, allow_nil: true}
 end
