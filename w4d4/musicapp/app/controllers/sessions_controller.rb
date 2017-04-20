83a293a32a54fc13054036e02d197e5452340b56
Leland Krych <Leland.Krych@gmail.com>
2017-04-20 14:24:57 -0700

    Add new, create, destroy methods to sessionscontroller, and general methods to applicationcontroller

--- w4d4/musicapp/app/controllers/sessions_controller.rb
+++ w4d4/musicapp/app/controllers/sessions_controller.rb
@@ -8,8 +8,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user.valid?
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      redirect_to new_sessions_url
+    end
   end
 
   def destroy
+    logout!(current_user)
+    redirect_to new_sessions_url
   end
 end
