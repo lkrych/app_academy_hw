c312be6669321e0eaebdff8e85f3c4e926ec3e4c
Leland Krych <Leland.Krych@gmail.com>
2017-04-23 17:46:17 -0700

    clean a04

--- w4d5/a04-prep-master/skeleton/app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-class Comment < ActiveRecord::Base
-  validates :body, :user, :link, presence: true
-
-  belongs_to :user
-  belongs_to :link
-end
--- w4d5/a04-prep-master/skeleton/config/routes.rb
+++ w4d5/a04-prep-master/skeleton/config/routes.rb
@@ -1,6 +1,2 @@
 Links::Application.routes.draw do
-  resources :users, only: [:new, :create]
-  resource :session, only: [:new, :create, :destroy]
-  resources :links
-  resources :comments, only: [:create, :destroy]
 end
