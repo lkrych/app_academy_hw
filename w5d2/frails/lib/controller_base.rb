b23735dd856120ca2c7331d59db51d3146c78976
Leland Krych <Leland.Krych@gmail.com>
2017-04-25 15:14:17 -0700

    Initial commit for frails, add redirect, render and render_content methods for base_controller

--- w5d2/frails/lib/controller_base.rb
+++ w5d2/frails/lib/controller_base.rb
@@ -2,31 +2,48 @@ require 'active_support'
 require 'active_support/core_ext'
 require 'erb'
 require_relative './session'
+require 'rack'
+require 'active_support/inflector'
 
 class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "You can only render once" if @already_built_response
+    @res.location = url
+    @res.status = 302
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "You can only render once" if @already_built_response
+    @res.body = [content]
+    @res.set_header 'Content-Type', content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = "views/#{self.class.name.underscore}/#{template_name}.html.erb"
+    new_template = ERB.new(File.read(path))
+    render_content(new_template.result(binding), 'text/html')
   end
 
   # method exposing a `Session` object
@@ -36,5 +53,5 @@ class ControllerBase
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
   end
-end
 
+end
