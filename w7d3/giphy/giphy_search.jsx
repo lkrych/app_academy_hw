7536ff0b053814929cf3f4b227d1723432b72112
Leland Krych <Leland.Krych@gmail.com>
2017-05-09 20:12:05 -0700

    create root component and load it on document load

--- w7d3/giphy/giphy_search.jsx
+++ w7d3/giphy/giphy_search.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {configureStore} from './store/store';
+import configureStore from './store/store';
 import Root from './components/root';
 
-import { fetchSearchGiphys } from './util/api_util';
-import { receiveSearchGiphys } from './actions/giphy_actions';
-
-const store = configureStore();
 
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  window.store = store;
-  window.fetchSearchGiphys = fetchSearchGiphys;
-  window.receiveSearchGiphys = receiveSearchGiphys;
+  const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
