7832acfc8e53289f8c07fac87f418979f44b6e1d
Leland Krych <Leland.Krych@gmail.com>
2017-05-09 20:04:18 -0700

    refactor Redux store

--- w7d3/giphy/store/store.js
+++ w7d3/giphy/store/store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import RootReducer from '../reducers/root_reducer';
 
-export const configureStore = () => {
-  return createStore(RootReducer);
+const configureStore = () => {
+  return createStore(RootReducer, applyMiddleware(thunk));
 };
+
+export default configureStore;
