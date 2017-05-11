dfd8f2ea2e5fb34d7cf50cc587c0ae5a3455c84a
Leland Krych <Leland.Krych@gmail.com>
2017-05-10 19:27:39 -0700

    Add actions for nested colors

--- w7d4/rainbow/entry.jsx
+++ w7d4/rainbow/entry.jsx
@@ -64,11 +64,11 @@ Rainbow = withRouter(Rainbow);
 const routes = (
   <Route path="/" component={Rainbow}>
     <Route path="red" component={Red} />
-      <Route path="orange" component={Orange} />
-      <Route path="yellow" component={Yellow} />
+      <Route path="red/orange" component={Orange} />
+      <Route path="red/yellow" component={Yellow} />
     <Route path="green" component={Green} />
     <Route path="blue" component={Blue} />
-      <Route path="indigo" component={Indigo} />
+      <Route path="blue/indigo" component={Indigo} />
     <Route path="violet" component={Violet} />
 
   </Route>
