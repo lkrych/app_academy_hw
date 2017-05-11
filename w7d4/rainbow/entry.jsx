c10adff53cb3ab6509cb703271829006c894348b
Leland Krych <Leland.Krych@gmail.com>
2017-05-10 19:29:20 -0700

    fix nested routes

--- w7d4/rainbow/entry.jsx
+++ w7d4/rainbow/entry.jsx
@@ -63,12 +63,14 @@ Rainbow = withRouter(Rainbow);
 
 const routes = (
   <Route path="/" component={Rainbow}>
-    <Route path="red" component={Red} />
-      <Route path="red/orange" component={Orange} />
-      <Route path="red/yellow" component={Yellow} />
+    <Route path="red" component={Red} >
+      <Route path="orange" component={Orange} />
+      <Route path="yellow" component={Yellow} />
+    </Route>
     <Route path="green" component={Green} />
-    <Route path="blue" component={Blue} />
-      <Route path="blue/indigo" component={Indigo} />
+    <Route path="blue" component={Blue} >
+      <Route path="indigo" component={Indigo} />
+    </Route>
     <Route path="violet" component={Violet} />
 
   </Route>
