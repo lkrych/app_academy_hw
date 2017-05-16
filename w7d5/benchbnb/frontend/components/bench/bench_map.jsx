ee15f6c44415f6516972a5ee48c54bbbedfbeb61
Leland Krych <Leland.Krych@gmail.com>
2017-05-15 17:20:24 -0700

    Add google maps to app

--- w7d5/benchbnb/frontend/components/bench/bench_map.jsx
+++ w7d5/benchbnb/frontend/components/bench/bench_map.jsx
@@ -4,9 +4,21 @@ class BenchMap extends React.Component {
   constructor(props){
     super(props);
   }
+
+  componentDidMount() {
+    // set the map to show SF
+    const mapOptions = {
+      center: { lat: 37.7758, lng: -122.435 }, // this is SF
+      zoom: 13
+    };
+
+    // wrap the mapDOMNode in a Google Map
+    this.map = new google.maps.Map(this.mapNode, mapOptions);
+  }
+
   render(){
     return (
-        <div id='map-container' ref='map'></div>
+        <div id='map-container' ref={ map => this.mapNode = map}></div>
     );
   }
 }
